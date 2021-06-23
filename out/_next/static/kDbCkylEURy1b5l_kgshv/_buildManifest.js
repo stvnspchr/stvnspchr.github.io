2bfb3dd2e53fe8f3ea8cddf3e1122df5a783414b
Steven Speicher <stevenspeicher@gmail.com>
2021-06-23 14:18:03 -0700

    Include exported files

---
+++ out/_next/static/kDbCkylEURy1b5l_kgshv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-5b674b0886167d78c759.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
